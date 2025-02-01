7530c1e3c081b443ad70c338d0b35b1baa9c2aa6
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-02-01 14:36:34 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-<<<<<<< HEAD
 const path = require("path");
-=======
->>>>>>> 9f689a3 (added google authentication,created backend,added toastify notification popup,editor page,template page)
+const path = require("path");
 
 // Initialize Express App
 const app = express();
@@ -17,11 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Load Firebase Service Account Key (Correct Path)
-<<<<<<< HEAD
 const serviceAccount = require(path.resolve(process.env.FIREBASE_ADMIN_SDK_PATH));
-=======
-const serviceAccount = require("./config/firebase-adminsdk.json");
->>>>>>> 9f689a3 (added google authentication,created backend,added toastify notification popup,editor page,template page)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -37,8 +31,5 @@ app.get("/", (req, res) => {
 // Start the Server
 app.listen(port, () => {
     console.log(`🚀 Server running at http://localhost:${port}`);
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 9f689a3 (added google authentication,created backend,added toastify notification popup,editor page,template page)
